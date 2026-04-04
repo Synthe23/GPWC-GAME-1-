9325207c86f7854b18953461dd3a5e0ef7117280
OmPrakash23 <mishraomm5000@gmail.com>
2026-04-04 09:56:02 +0530

    created the updateBranches function and the branchPos array, branch array

--- Timber.cpp
+++ Timber.cpp
@@ -1,1 +1,17 @@
+#include <SFML/Graphics.hpp>
+#include <sstream>
+#include <SFML/Audio.hpp>
 
+using namespace sf;
+
+void updateBranches(int seed);
+
+const int NUM_BRANCHES = 6;
+enum class side
+{
+    LEFT,
+    RIGHT,
+    NONE
+};
+side branchPositions[NUM_BRANCHES];
+Sprite Branches[NUM_BRANCHES];
